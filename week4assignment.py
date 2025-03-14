69f492377fb1b159f49e0d19f5ea074ae31056c4
salamPhard <salamfadilu@gmail.com>
2025-03-14 14:41:39 +0100

    edited week 4 assignment

--- week4assignment.py
+++ week4assignment.py
@@ -1,23 +1,35 @@
-try:
+def readfromfile(file):
+    try:
     #open first file for reading
-    with open('welcome.txt', 'r') as file:
-        data = file.read() #read the file
+        with open(file, 'r') as initialfile:
+            data = initialfile.read() #read the file
+            #print(data)
+    except FileNotFoundError:
+        print("File not found")
+    initialfile.close()
+    return data
 
+def modify_content(file):
     #Writing a modified version of the file
-    with open('myNewText.txt', 'w') as file2:
-        edit_data = file2.write(data + " now I am adding a modified version")
+    original_file = readfromfile(file)
+    modified_data = original_file.upper()
+    return modified_data
 
-    #Reading the modified version
-    with open('myNewText.txt', 'r') as modified_data:
-        print(modified_data.read())
 
-#Handle the errors
-except FileNotFoundError:
-    print("File not found. please check the filename")
-except Exception as e:
-    print(f"error occurred: {e}")
+def write_new_file(file, new_created_file):
+    try:
+        #Writing the modified version
+        with open(new_created_file, 'w') as modified_data:
+            new_file = modified_data.write(file + " I AM NOW MODIFIED AND WRITTEN TO FILE")
+            print("File written and modified successfully")
+    except FileExistsError:
+        print("File already exist")
+        modified_data.close()
+        
+    return new_file
 
-finally:
-    file.close()
-    file2.close()
-    modified_data.close()
+
+
+readfromfile('welcome.txt')
+new_file = modify_content('welcome.txt')
+write_new_file(new_file, 'finaltext.txt')
